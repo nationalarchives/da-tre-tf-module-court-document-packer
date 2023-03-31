0ec877ad4ebcd5cd7d079679a70a2d8b6bda6837
rahmedzaizi <rahmed@zaizi.com>
2023-03-31 17:23:16 +0100

    sf log-group name prefixed with /aws/vendedlogs/states/

--- cloudwatch_log_group.tf
+++ cloudwatch_log_group.tf
@@ -1,3 +1,3 @@
 resource "aws_cloudwatch_log_group" "judgment_packer" {
-  name = "${var.env}-${var.prefix}-judgment-packer"
+  name = "/aws/vendedlogs/states/${var.env}-${var.prefix}-judgment-packer"
 }
