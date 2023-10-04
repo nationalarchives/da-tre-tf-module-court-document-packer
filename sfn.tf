4165b923a2abd046855b630e800d62f41221b951
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-10-04 11:48:16 +0100

    Publish to da eventbus rather than tre internal

--- sfn.tf
+++ sfn.tf
@@ -4,7 +4,7 @@ resource "aws_sfn_state_machine" "court_document_pack_sf" {
   definition = templatefile("${path.module}/templates/step-function-definition.json.tftpl", {
     arn_lambda_court_document_pack            = aws_lambda_function.court_document_pack.arn
     arn_sns_topic_tre_slack_alerts            = var.common_tre_slack_alerts_topic_arn
-    arn_sns_topic_tre_court_document_pack_out = var.common_tre_internal_topic_arn
+    arn_sns_topic_tre_court_document_pack_out = var.common_da_eventbus_topic_arn
   })
   logging_configuration {
     log_destination        = "${aws_cloudwatch_log_group.court_document_pack.arn}:*"
